3f385c0571a9764a5f44d4f5ac6760d28d932a17
aanimesh23 <animesha@uci.edu>
2020-01-19 21:04:12 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Animesh Agrawal on 1/19/20.
+//  Copyright © 2020 Animesh Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
